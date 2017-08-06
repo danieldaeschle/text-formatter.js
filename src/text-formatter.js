b8e36320b14f3d3c3b61957726fa2c344c2937ee
danieldaeschle <daniel.daeschle@gmail.com>
2017-08-06 13:48:18 +0200

    created dist folder

--- src/text-formatter.js
+++ src/text-formatter.js
@@ -9,12 +9,12 @@ var textSelected = false;
 var isTextFormatterVisible = false;
 /** @const {string} */
 var textFormatterTooltipElement = '<div id="highlightTooltip">'+
-                            '<button onclick="document.execCommand(\'bold\', false, null)"><i class="material-icons">format_bold</i></button>'+
-                            '<button onclick="document.execCommand(\'italic\', false, null)"><i class="material-icons">format_italic</i></button>'+
-                            '<button onclick="document.execCommand(\'underline\', false, null)"><i class="material-icons">format_underline</i></button>'+
-                            '<button onclick="changeSize(true)"><i class="material-icons">format_size</i><i class="material-icons">arrow_drop_up</i></button>'+
-                            '<button onclick="changeSize(false)"><i class="material-icons">format_size</i><i class="material-icons">arrow_drop_down</i></button>'+
-                            '</div>'
+                                    '<button onclick="document.execCommand(\'bold\', false, null)"><i class="material-icons">format_bold</i></button>'+
+                                    '<button onclick="document.execCommand(\'italic\', false, null)"><i class="material-icons">format_italic</i></button>'+
+                                    '<button onclick="document.execCommand(\'underline\', false, null)"><i class="material-icons">format_underline</i></button>'+
+                                    '<button onclick="changeSize(true)"><i class="material-icons">format_size</i><i class="material-icons">arrow_drop_up</i></button>'+
+                                    '<button onclick="changeSize(false)"><i class="material-icons">format_size</i><i class="material-icons">arrow_drop_down</i></button>'+
+                                    '</div>'
 
 /**  Writes the required HTML DOM into the document */
 window.onload = (function() {
